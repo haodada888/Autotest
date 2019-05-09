bb2637060817d867149f2cde07961885e572a4df
root <root@localhost.localdomain>
2019-05-09 16:28:52 +0800

    1

---
+++ jartest/src/main/java/testjar/TestA.java
@@ -0,0 +1,11 @@
+package testjar;
+
+public class TestA {
+
+
+    public static void main(String args[]){
+        TestB hao=new TestB();
+        hao.test1();
+    }
+
+}
---
+++ jartest/src/main/java/testjar/TestB.java
@@ -0,0 +1,13 @@
+package testjar;
+
+public class TestB {
+
+    int a=1;
+    int b=2;
+
+    public void  test1(){
+        int c=a+b;
+
+        System.out.println(c);
+    }
+}
